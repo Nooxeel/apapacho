768209112f4e345bd5adee3496800bef405b92bd
Nooxeel <jorge.moya@live.com>
2026-01-23 12:40:55 -0300

    perf: disable Vercel image optimization to avoid costs

--- next.config.js
+++ next.config.js
@@ -29,6 +29,7 @@ const nextConfig = {
   compress: true,
   
   images: {
+    unoptimized: true, // Disable Vercel image optimization to avoid costs
     remotePatterns: [
       {
         protocol: 'https',
@@ -59,9 +60,6 @@ const nextConfig = {
         hostname: 'res.cloudinary.com',
       },
     ],
-    formats: ['image/webp', 'image/avif'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   async headers() {
     return [
